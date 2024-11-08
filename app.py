8be6adaaf2ab9e819fa30810ec72f3cb5eef640f
marsyafrdlh <marsya.faradilah15@gmail.com>
2024-11-08 17:55:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,6 @@ import ssl
 from urllib.request import urlopen
 
 
-# Define preprocessing
-preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-
 # Load ImageNet class labels
 LABELS_URL = "https://raw.githubusercontent.com/anishathalye/imagenet-simple-labels/master/imagenet-simple-labels.json"
 @st.cache
