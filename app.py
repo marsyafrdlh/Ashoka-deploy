09f958324ce6d1fdf8ebe45eff99c310065b2607
marsyafrdlh <marsya.faradilah15@gmail.com>
2024-11-08 19:30:45 +0700

    update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from PIL import Image,ImageOps
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import tensorflow_hub as hub
 import torch
 import numpy as np
 import cv2
