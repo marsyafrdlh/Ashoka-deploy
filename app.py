4c47398be31296dc743d7e70e02038439e8f6d48
marsyafrdlh <marsya.faradilah15@gmail.com>
2025-01-02 20:38:53 +0700

    update3

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from PIL import Image
 import matplotlib.pyplot as plt
-import torch
-import torchvision.transforms as transforms
+import tensorflow as tf
 import numpy as np
+from tensorflow.keras.preprocessing import image as keras_image
 
 # Streamlit app
 st.title("Hypospadias Image Classification")
@@ -12,31 +12,36 @@ st.write("Upload an image to classify using a pretrained model.")
 # Fungsi untuk memuat model
 @st.cache_resource
 def load_model():
-    model_path = "model.pth"  # Ganti dengan path model PyTorch Anda
-    model = torch.load(model_path, map_location=torch.device('cpu'))
-    model.eval()  # Set model ke evaluasi mode
+    # Muat model dari file .h5
+    model_path = r'/content/drive/Mydrive/Ashoka dataset/model.h5'  # Ganti dengan path model .h5 Anda
+    model = tf.keras.models.load_model(model_path)
     return model
 
 # Fungsi untuk preprocessing gambar
 def preprocess_image(image):
-    transform = transforms.Compose([
-        transforms.Resize((128, 128)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5], std=[0.5])  # Sesuaikan dengan preprocessing saat melatih model
-    ])
-    image = transform(image).unsqueeze(0)  # Tambahkan batch dimension
-    return image
+    test_image = image.resize((128, 128))  # Sesuaikan dengan ukuran input model
+    test_image = keras_image.img_to_array(test_image)  # Mengubah gambar menjadi array
+    test_image = test_image / 255.0  # Normalisasi pixel gambar (0-1)
+    test_image = np.expand_dims(test_image, axis=0)  # Tambahkan dimensi batch
+    return test_image
 
 # Fungsi untuk prediksi kelas
 def predict_class(image, model):
     classnames = ['normal', 'abnormal']  # Sesuaikan dengan label kelas Anda
     processed_image = preprocess_image(image)
-    with torch.no_grad():
-        outputs = model(processed_image)
-        _, predicted = torch.max(outputs, 1)
-        class_idx = predicted.item()
-        confidence = torch.softmax(outputs, dim=1)[0][class_idx].item() * 100
-    return f"Class: {classnames[class_idx]}, Confidence: {confidence:.2f}%"
+    
+    # Prediksi menggunakan model
+    predictions = model.predict(processed_image)
+    
+    # Softmax untuk mendapatkan probabilitas kelas
+    scores = tf.nn.softmax(predictions[0])
+    scores = scores.numpy()  # Convert ke numpy array untuk manipulasi lebih lanjut
+    
+    # Menentukan kelas dengan probabilitas tertinggi
+    image_class = classnames[np.argmax(scores)]
+    confidence = scores[np.argmax(scores)] * 100  # Confidence dalam persen
+    
+    return f"Class: {image_class}, Confidence: {confidence:.2f}%"
 
 # Fungsi utama aplikasi Streamlit
 def main():
